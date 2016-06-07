7a362fd9bd9a492cb40397972dfaec40acfbd372
Timothy Lim <tim.lim@intercom.io>
2016-06-07 23:40:16 +0800

    Add icons and better styling

--- manifest.php
+++ manifest.php
@@ -12,4 +12,4 @@ lib/jquery/jquery.min.js
 lib/underscore/underscore-min.js
 NETWORK:
 *
-<?php   echo "# Hash: " . md5_file('index.htm') . md5_file('js/app.js') .  "\n"; ?>
+<?php   echo "# Hash: " . md5_file('index.htm') . md5_file('js/app.js') . md5_file('css/style.css') .  "\n"; ?>
